d847f7fa4617706e7d3ca31ca0ba996fd6116833
langj123 <88846488+web-redegg-marketing@users.noreply.github.com>
2022-11-30 15:20:53 -0700

    npm run build

--- support/blocks/index.js
+++ support/blocks/index.js
@@ -3,46 +3,16 @@ import "../scss/editor.scss";
 import "../core/styles.js";
 
 // Format Types
-import "../format-types/rotate-words";
+// import "../format-types/rotate-words";
 import "../format-types/background-gradient";
-import "../format-types/block-element";
 
 // Core Blocks
 import "./core";
 import "./core/button";
 
 // Custom Blocks
-// import "./hero";
-import "./text-card";
-import "./section-header";
-import "./text-cards-grid";
-import "./title-image-cta";
-import "./title-image-cta-group";
+import "./hero";
 import "./header-intro";
-import "./header-intro-column";
+import "./columns-group";
 import "./image-text-columns";
-import "./contact";
-import "./contact-content";
-import "./contact-forms";
-import "./shortcode-section";
-// import "./faq";
-// import "./faq-section";
-// import "./columns-list";
-import "./selected-projects";
-import "./checklist";
-import "./tabs";
-import "./tab";
-import "./tab-group";
-import "./image-links";
-import "./image-link";
-import "./image-swiper";
-import "./stat-callout";
-import "./progress-block";
-import "./case-study";
-import "./selected-case-study";
-import "./selected-case-studies";
-import "./savings-calculator";
-import "./savings-form";
-import "./cta";
-import "./filter-resources";
-import "./feature-section";
+import "./text-cards-grid";
